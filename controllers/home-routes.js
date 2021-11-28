74094026f2176aa0817ec323e202770b6743e105
Ian Holmes <concord511@gmail.com>
2021-11-28 16:36:24 -0600

    created several templates and scripts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -95,4 +95,13 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// sign up page route
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+})
+
 module.exports = router;
