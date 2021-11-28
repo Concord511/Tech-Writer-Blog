72145c7a4ea0e926c5a6b0a1a4ed2ad4429c59f3
Ian Holmes <concord511@gmail.com>
2021-11-28 15:16:12 -0600

    added utils folder and method to determine session status

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+// method to determine session status
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+        // if there is no user_id in the session redirect to login page
+        res.redirect('/login');
+    }
+    else {
+        next();
+    }
+};
+
+module.exports = withAuth;
