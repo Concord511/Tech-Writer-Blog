b52ea50dae2f16dbb8b1b55158b81417e3cffa11
Ian Holmes <concord511@gmail.com>
2021-11-28 15:22:59 -0600

    added update and delete post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
-/*
-    Will add authoriztion
-    const withAuth = require('../../utils/auth');
-*/
+const withAuth = require('../../utils/auth');
 
 // get all Posts
 router.get('/', (req, res) => {
@@ -87,3 +84,65 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// create Post
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// update Post
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostDat);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// delete post
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
